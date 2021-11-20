58b5befbe350a1e02be81add5aea9360acc08769
André Diego Piske <andrepiske@gmail.com>
2021-11-20 17:28:19 +0100

    Fix "edit this page" link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/apiske/webhooksuno_doc/edit/main/',
+          editUrl: 'https://github.com/apiske/webhooksuno_website/edit/main/',
         },
         blog: false,
         theme: {
